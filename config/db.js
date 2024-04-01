3e596d6e497a6f5a266f20446ef35b6726dd63b2
Rohan Raman <rohan.raman@vvdntech.in>
2024-04-01 15:32:41 +0530

    Minor changes in readme, config and .env file for credential management of mongoDB

--- config/db.js
+++ config/db.js
@@ -5,8 +5,9 @@ const connectDB = async () => {
     try {
         const username = process.env.MONGODB_USERNAME;
         const password = process.env.MONGODB_PASSWORD; 
+        const mongoURI = process.env.MONGODB_URI
         // console.log("UserName=>", username, "password=>", password, `mongodb://${username}:${password}@127.0.0.1:27017/test-project`)
-        await mongoose.connect(`mongodb://${username}:${password}@localhost:27017/test-project`);
+        await mongoose.connect(`mongodb://${username}:${password}@${mongoURI}/test-project`);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err.message);
